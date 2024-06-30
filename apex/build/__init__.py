b55371646f1512a0d6f1aeaab689a3aa82940c31
n25a <nimashadix@gmail.com>
2024-07-01 02:27:26 +0330

    🚊 update something

--- apex/build/__init__.py
+++ apex/build/__init__.py
@@ -1,4 +1,3 @@
 from apex.build.utils import get_blueprint, VSCType
-from apex.build.apex_syntax import parse_apex_syntax
-from apex.build.questions import ask
-from apex.build.generate_project import generate_project
+from apex.build.apex_syntax import Apex
+from apex.build.questions import ask_questions
