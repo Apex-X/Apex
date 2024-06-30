6b2f6dc2a1fed4af5bef50bd98edb63655fd8a30
nima.mashhadi <nima.mashhadi@snapp.cab>
2024-06-30 20:54:09 +0330

    🪷 update function names

---
+++ apex/build/__init__.py
@@ -0,0 +1,4 @@
+from apex.build.utils import get_blueprint, VSCType
+from apex.build.apex_syntax import parse_apex_syntax
+from apex.build.questions import ask
+from apex.build.generate_project import generate_project
---
+++ apex/cmd/__init__.py
@@ -0,0 +1,2 @@
+from apex.cmd.root import cli
+from apex.cmd.header import header
