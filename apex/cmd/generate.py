1309314e0df046879bbab67907b86f7d35a038b1
n25a <nimashadix@gmail.com>
2024-07-14 23:04:58 +0330

    🫂 remove feature getting project from GitHub or Gitlab

--- apex/cmd/generate.py
+++ apex/cmd/generate.py
@@ -4,52 +4,22 @@ import click
 
 @click.command("generate")
 @click.option(
-    '--git_token',
+    '--blueprint_path',
     type=str,
     default="",
     multiple=False,
-    help='git token to get template'
+    help='path of local project template'
 )
-@click.option(
-    '--vcs_type',
-    type=str,
-    default="",
-    multiple=False,
-    help='version control system'
-)
-@click.option(
-    '--template_id',
-    type=int,
-    default=0,
-    multiple=False,
-    help='git template code'
-)
-@click.option(
-    '--local_template_path',
-    type=str,
-    default="",
-    multiple=False,
-    help='path of local template'
-)
-def generate(git_token: str, vcs_type: str, template_id: int, local_template_path: str):
+def generate(blueprint_path: str):
     """
     Generate project from template in VCS or local path template\n
 
-    [For generating project from VCS]:\n
-    $ apex generate --git_token "YOUR-TOKEN" --vcs_type "github" --template_id 12456 \n
-
     [For generating project from local path template]:\n
-    $ apex generate --local_template_path "YOUR-TEMPLATE-PATH" \n
+    $ apex generate --blueprint_path "YOUR-TEMPLATE-PATH" \n
     """
 
     apex = Apex()
-    if local_template_path:
-        tags, vars_to_tag = apex.parse_syntax(local_template_path)
+    if blueprint_path:
+        tags, vars_to_tag = apex.parse_syntax(blueprint_path)
         selected_tags, selected_vars, destination_path, project_name = ask_questions(tags, vars_to_tag)
-        apex.generate_project(local_template_path, destination_path, project_name, selected_tags, selected_vars)
-    else:
-        tmp = get_blueprint(git_token, vcs_type, template_id)
-        tags, vars_to_tag = apex.parse_syntax(tmp.name)
-        selected_tags, selected_vars, destination_path, project_name = ask_questions(tags, vars_to_tag)
-        apex.generate_project(tmp.name, destination_path, project_name, selected_tags, selected_vars)
-        tmp.cleanup()
+        apex.generate_project(blueprint_path, destination_path, project_name, selected_tags, selected_vars)
